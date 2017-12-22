438c530461a033c5700001dc43c927a83f7b5ef0
Allan Emerson <allan@electricpulp.com>
2017-12-21 20:05:02 -0700

    adds video support

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -30,113 +30,201 @@ $(document).ready(function() {
   slate.a11y.pageLinkFocus($(window.location.hash));
 
   $('.in-page-link').on('click', function(evt) {
-    slate.a11y.pageLinkFocus($(evt.currentTarget.hash));
+	slate.a11y.pageLinkFocus($(evt.currentTarget.hash));
   });
 
   // Target tables to make them scrollable
   var tableSelectors = '.rte table';
 
   slate.rte.wrapTable({
-    $tables: $(tableSelectors),
-    tableWrapperClass: 'rte__table-wrapper',
+	$tables: $(tableSelectors),
+	tableWrapperClass: 'rte__table-wrapper',
   });
 
   // Target iframes to make them responsive
   var iframeSelectors =
-    '.rte iframe[src*="youtube.com/embed"],' +
-    '.rte iframe[src*="player.vimeo"]';
+	'.rte iframe[src*="youtube.com/embed"],' +
+	'.rte iframe[src*="player.vimeo"]';
 
   slate.rte.wrapIframe({
-    $iframes: $(iframeSelectors),
-    iframeWrapperClass: 'rte__video-wrapper'
+	$iframes: $(iframeSelectors),
+	iframeWrapperClass: 'rte__video-wrapper'
   });
 
   // Apply a specific class to the html element for browser support of cookies.
   if (slate.cart.cookiesEnabled()) {
-    document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies');
+	document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies');
   }
 
   // off canvas nav
   $('body').on('click', '.navbar-toggle', function(e){
-    e.preventDefault();
-    $('body').toggleClass('menu-open');
-    LS.overlay.open();
+	e.preventDefault();
+	$('body').toggleClass('menu-open');
+	LS.overlay.open();
   });
   $('body').on('click', '#nav-window-close', function(e){
-    e.preventDefault();
-    $('body').removeClass('menu-open');
-    LS.overlay.close();
+	e.preventDefault();
+	$('body').removeClass('menu-open');
+	LS.overlay.close();
   });
   $('.site-content').on('click', function(e){
-      if( $('body').hasClass('menu-open') ){
-        e.stopPropagation();
-        $('body').removeClass('menu-open');
-        LS.overlay.close();
-      }
+	  if( $('body').hasClass('menu-open') ){
+		e.stopPropagation();
+		$('body').removeClass('menu-open');
+		LS.overlay.close();
+	  }
   });
   $('body').on('click', '.menu-toggle', function(e){
-    e.preventDefault();
-    $(this).closest('li').toggleClass('open');
+	e.preventDefault();
+	$(this).closest('li').toggleClass('open');
   });
 
   // sticky sidebar
   var has_sticky_element = $('.sticky').length > 0;
 
   if( has_sticky_element ){
-    var el_starting_pos = $('.sticky').offset().top;
-    if( $(window).width() > LS.desktopBreakpoint ){
-      LS.filters.setStickyColumnHeight($('.main'));
-    }
+	var el_starting_pos = $('.sticky').offset().top;
+	if( $(window).width() > LS.desktopBreakpoint ){
+	  LS.filters.setStickyColumnHeight($('.main'));
+	}
   }
 
   $(window).load( function() { // do this after the load so we have our images
-    if( $(window).width() > LS.desktopBreakpoint && has_sticky_element ){
-      LS.doStickyColumn(el_starting_pos);
-    }
+	if( $(window).width() > LS.desktopBreakpoint && has_sticky_element ){
+	  LS.doStickyColumn(el_starting_pos);
+	}
   });
 
   $(window).scroll(function(){
-    if( $(window).width() > LS.desktopBreakpoint && has_sticky_element ){
-      LS.doStickyColumn(el_starting_pos);
-    }
+	if( $(window).width() > LS.desktopBreakpoint && has_sticky_element ){
+	  LS.doStickyColumn(el_starting_pos);
+	}
   });
 
   // Smooth scrolling
   $('body').on('click', '.smoothscroll, .nav-local a, .page-nav-item, .scroll-down-indicator', function(e){
-    e.preventDefault();
+	e.preventDefault();
 
-    var $btn = $(this),
-      selector = $btn.attr('href'),
-      pos = 0;
+	var $btn = $(this),
+	  selector = $btn.attr('href'),
+	  pos = 0;
 
-    // If the target isn't an on-page anchor, treat it like a normal link
-    if (!selector.match(/^#/)) {
-      window.location = selector;
-      return;
-    }
+	// If the target isn't an on-page anchor, treat it like a normal link
+	if (!selector.match(/^#/)) {
+	  window.location = selector;
+	  return;
+	}
 
-    $target = $(selector);
-    if ($target.length === 0) {
-      $target = $('body');
-    }
+	$target = $(selector);
+	if ($target.length === 0) {
+	  $target = $('body');
+	}
 
-    pos = $target.offset().top;
-    $('html,body').animate({scrollTop: pos+'px'});
+	pos = $target.offset().top;
+	$('html,body').animate({scrollTop: pos+'px'});
 
   });
 
   // marquees
   $('body').on('click', '.marquee-close', function(e){
-    e.preventDefault();
-    $('#shopify-section-marquee').fadeOut('fast');
-    sessionStorage.setItem("lo-marquee-dismissed", "1");
+	e.preventDefault();
+	$('#shopify-section-marquee').fadeOut('fast');
+	sessionStorage.setItem("lo-marquee-dismissed", "1");
   });
 
   // marquees are hidden by default, so show if they haven't been closed
   if ( 'lo-marquee-dismissed' in sessionStorage) {
-    // no nothing
+	// no nothing
   }else{
-    $('#shopify-section-marquee .marquee').removeClass('hidden');
+	$('#shopify-section-marquee .marquee').removeClass('hidden');
   }
+
+  /*
+	Videos
+   */
+	var $videos = $('.embed-container');
+	$videos.each(function(){
+		var $embed = $(this);
+		var videoId = $(this).find('video').attr('id');
+		var video = document.getElementById(videoId);
+		var autoPlay = $(this).data('autoplay');
+		var loops = $(this).data('loops');
+		var videoUrl = $embed.data('desktop-url');
+		var mobileVideoUrl = $embed.data('mobile-url');
+
+		if ( $(window).width() < LS.desktopBreakpoint && mobileVideoUrl ) {
+	        $('#'+videoId).append('<source src="' + mobileVideoUrl + '" type="video/mp4" />');
+	    } else {
+	        $('#'+videoId).append('<source src="' + videoUrl + '" type="video/mp4" />');
+	    }
+
+		video.oncanplay = function() {
+			$embed.find('.loading').fadeOut();
+			// play it if we can see it 
+			if( LS.isElementInViewport(video) && $embed.data('autoplay') ){
+				var playPromise = video.play();
+				if (playPromise !== undefined) {
+					playPromise.then(function() {
+						// Automatic playback started, nothing to do
+					}).catch(function(error) {
+						console.log('Playback did not start. Reason: ' + error)
+					});
+				}
+			}
+		};
+
+		// play/pause based on visibility
+		$(window).scroll(function(){
+			if( LS.isElementInViewport(video) ){
+				if( !$embed.hasClass('ended') && $embed.data('autoplay') ){
+					var playPromise = video.play();
+					if (playPromise !== undefined) {
+						playPromise.then(function() {
+							// Automatic playback started, nothing to do
+						}).catch(function(error) {
+							console.log('Playback did not start. Reason: ' + error)
+						});
+					}
+				}
+			}else{
+				var pausePromise = video.pause();
+				if (pausePromise !== undefined) {
+					pausePromise.then(function() {
+						// Paused, nothing to do
+					}).catch(function(error) {
+						console.log('Pause error. Reason: ' + error)
+					});
+				}
+			}
+		});
+
+		// limit number of plays and maybe hide overlay text
+		var playCount= 0;
+		var lastTime = 0;
+		video.addEventListener("timeupdate", function() {
+			if( playCount === loops ){
+				video.pause();
+				$embed.addClass('ended');
+				$embed.find('.video-trigger').fadeIn('slow');
+			}
+
+			// catching the start of the video is tricky with the loop, so see if the current time is the last time we saved ?>
+			if( video.currentTime < lastTime ){
+				playCount++;
+				lastTime = 0;
+			}else{
+				lastTime = video.currentTime;
+			}
+		}, true);
+
+		// that was cool, play it again ?>
+		$embed.find('.video-trigger').on('click', function(e){
+			e.preventDefault();
+			$(this).fadeOut('fast');
+			$embed.removeClass('ended');
+			playCount = 0;
+			video.play();
+		});
+	});
 
 });
