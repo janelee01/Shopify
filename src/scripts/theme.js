775a1879a5d947a0a094f4a2a460fdbd99669f46
Allan Emerson <allan@electricpulp.com>
2018-01-22 18:16:10 -0700

    adds product gallery sliders

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -22,10 +22,12 @@ window.theme = window.theme || {};
 
 /*================ Lo & Sons ================*/
 // =require vendor/bootstrap.min.js
+// =require vendor/jquery-ui-1.10.4.custom.min.js
 // =require vendor/moment.min.js
 // =require vendor/jquery.fitvids.js
 // =require vendor/jquery.cookie.js
 // =require vendor/jquery.touchSwipe.min.js
+// =require vendor/jquery.film_roll.js
 // =require vendor/lo-and-sons.js
 // =require vendor/product.js  
 
@@ -428,10 +430,10 @@ $(document).ready(function() {
 	    $(this).addClass('active');
 
 	    // replace galleries
-	    $('#product-galleries .sibling-content').each(function(){
-	    	$(this).addClass('hide');
+	    $('.product-gallery').removeClass('active');
+	    $('.product-gallery').each(function(){
 	    	if( $(this).data('sibling') == siblingId ){
-	    		$(this).removeClass('hide');
+	    		$(this).addClass('active');
 	    	}
 	    });
 
@@ -587,6 +589,71 @@ $(document).ready(function() {
 		$('.write-form .yotpo-header-title').text('Write your review');
 		$('.write-form .yotpo-submit').val('Submit review');
 	    $(this).addClass('is-hidden');
+	});
+
+	var animation_speed = 800;
+	if( $(window).width() < LS.desktopBreakpoint ){
+		animation_speed = 500;
+	}
+
+	$('.product-gallery').each(function(){
+
+		var galleryKey = $(this).find('.filmroll').attr('id');
+		var galleryId = '#' + galleryKey;
+
+		var fr = new FilmRoll({
+			container: galleryId,
+			scroll: false,
+			hover: false, // must be false to allow clickable slides on
+			animation: animation_speed,
+			easing: 'easeInOutCubic',
+			prev: '#gallery-' + $(this).data('color-id') + '-prev',
+			next: '#gallery-' + $(this).data('color-id') + '-next',
+			vertical_center: false,
+			no_css: true
+		});
+
+		LS.galleries[galleryKey] = fr;
+
+		// fix the image heights on resize
+		var $gallery = $(galleryId),
+			$masterImage = $gallery.find('.master img');
+
+		if($masterImage){
+			$gallery.on('film_roll:resizing', function(){
+				$gallery.find('.resize img').height($masterImage.height()); 
+			});
+		}
+
+		$gallery
+			.on('film_roll:moved', function(event) {
+				// gallery (maybe) shifts to index 0 on load, so hide our loading state when it's done
+				$('.gallery-container').removeClass('is-loading');
+				$('.gallery-container .loading').fadeOut();
+			})
+			.on('film_roll:activate', function(event){ // done loading
+				LS.toggleGalleryControls($gallery);
+				// if the gallery doesn't need to move because it's narrower than the window, show it when ready
+				// shuttle will be twice the width of the child elements
+				if( $gallery.find('.film_roll_shuttle').width()/2 < $(window).width() ){
+					$('.gallery-container').removeClass('is-loading');
+					$('.gallery-container .loading').fadeOut();
+				}
+			})
+			.on('film_roll:resized', function(event){ // window size changes
+				LS.toggleGalleryControls($gallery);
+			});
+
+		if ( LS.haveAnalytics() ) {
+			$gallery.on('film_roll:moved', function(event) {
+			    var $active_slide = $gallery.find('.film_roll_pager a.active');
+			    	active_slide_position = $gallery.find('.film_roll_pager a').index( $active_slide ) + 1; // using the pager here because film role adjusts the index of slides with each animation
+			    	// the view for the first slide will be triggered by the swatch click
+			    	if( active_slide_position > 1 ){
+			    		ga('send', 'event', 'pdp_image_simple', 'view', LS.getProductName() + ' - ' + LS.getProductVariant() + ' - ' + active_slide_position);
+			    	}
+			});
+		}
 	});
 
 	/*
