871f0d9ca9ae05be459bf9a01ee1703b5b3adeef
Max Rolon <max.rolon@barrelny.com>
2018-12-20 11:26:16 -0500

    Prevents flip to page nav if mega menu active

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -223,6 +223,7 @@ $(document).ready(function() {
 		var isPastTrigger = $(window).scrollTop() > trigger
 		var isMovingDown = $(window).scrollTop() > lastScrollPos
 		var isMovingUp = $(window).scrollTop() < lastScrollPos
+		var isMegaMenuActive = $('body').hasClass('mega-menu-active')
 		var marqueeheight = $marquee.outerHeight()
 		var isPageNav = $pageNav.length
 		var isMarquee = $marquee.is(':visible')
@@ -266,7 +267,9 @@ $(document).ready(function() {
 		// No Marquee
 		// Moving Down
 		if (isPageNav && !isMarquee && isMovingDown && isPastTrigger) {
-			header.css({'top' : $('#site-header-items').outerHeight() * -1});
+			if (!isMegaMenuActive) {
+				header.css({'top' : $('#site-header-items').outerHeight() * -1});
+			}
 			return
 		}
 
@@ -282,8 +285,10 @@ $(document).ready(function() {
 		// Marquee
 		// Moving Down
 		if (isPageNav && isMarquee && isPastTrigger && isMovingDown) {
-			$header.css({'top' : ($marquee.outerHeight() + $('#site-header-items').outerHeight()) * -1});
-			$('.js-marque-push-down').addClass('reset-marquee-offset');
+			if (!isMegaMenuActive) {
+				$header.css({'top' : ($marquee.outerHeight() + $('#site-header-items').outerHeight()) * -1});
+				$('.js-marque-push-down').addClass('reset-marquee-offset');
+			}
 			return
 		}
 		
