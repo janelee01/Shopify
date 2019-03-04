7933100f61c695f63e6681debd8479f496a94282
Wil Bunge <wilfredo.b@barreldev.com>
2019-03-04 13:17:11 -0300

    Adds pdp-callout-galleries.

--- src/scripts/sections/pdp-callout-galleries.js
+++ src/scripts/sections/pdp-callout-galleries.js
@@ -1,3 +1,21 @@
+// import Flickity from 'flickity'
+
 export default el => {
+  // 1st carousel, main
+
   // Add in module JS code here
+  // el.classList.add('wilcho');
+  // element argument can be a selector string
+  // for an individual element
+
+  var $main = el.querySelector('.pdp-product-features-carousel-main')
+  var $nav = el.querySelector('.pdp-product-features-carousel-nav')
+
+  var flkty1 = new Flickity($main)
+
+  var flkty2 = new Flickity($nav, {
+    asNavFor: '.pdp-product-features-carousel-main',
+    contain: true,
+    pageDots: false
+  })
 }
