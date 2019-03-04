cc10271d5c882b0183945ebae286bdb7444ed8c4
Wil Bunge <wil@avanfeel.com>
2019-03-04 00:54:50 -0300

    PDP-TESTIMONIALS module

--- src/scripts/sections/pdp-testimonials.js
+++ src/scripts/sections/pdp-testimonials.js
@@ -1,3 +1,21 @@
+// import Flickity from 'flickity'
+
 export default el => {
+  // 1st carousel, main
+
   // Add in module JS code here
+  // el.classList.add('wilcho');
+  // element argument can be a selector string
+  // for an individual element
+
+  var $main = el.querySelector('.pdp-testimonials-texts')
+  var $nav = el.querySelector('.pdp-testimonials-logos')
+
+  var flkty1 = new Flickity($main)
+
+  var flkty2 = new Flickity($nav, {
+    asNavFor: '.pdp-testimonials-texts',
+    contain: true,
+    pageDots: false
+  })
 }
