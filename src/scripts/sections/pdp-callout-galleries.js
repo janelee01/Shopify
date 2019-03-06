9a92f0a3ad6c8feab8bc30512494f2bc2ef27fc9
Max <max.rolon@barrelny.com>
2019-03-06 17:58:03 -0500

    Updates pdp gallery to use as-nav-for

--- src/scripts/sections/pdp-callout-galleries.js
+++ src/scripts/sections/pdp-callout-galleries.js
@@ -1,19 +1,27 @@
 import Flickity from 'flickity'
 import AsNavFor from 'flickity-as-nav-for'
-import Sync from 'flickity-sync'
 
 export default el => {
-  var $main = el.querySelector('.pdp-product-features-carousel-main')
-  var $nav = el.querySelector('.pdp-product-features-carousel-nav')
+  const $main = el.querySelector('.pdp-product-features-carousel-main')
+  const $nav = el.querySelector('.pdp-product-features-carousel-nav')
 
-  var flktyMain = new Flickity($main, {
+  const flktyMain = new Flickity($main, {
     prevNextButtons: false,
     pageDots: false,
     contain: true
   })
 
-  var flktyNav = new Flickity($nav, {
+  const flktyNav = new Flickity($nav, {
+    asNavFor: $main,
     contain: true,
     pageDots: false
+  })
+
+  flktyNav.on('change', (index) => {
+    setTimeout(() => {
+      if (flktyMain.selectedIndex !== index) {
+        flktyMain.select(index)
+      }
+    }, 0)
   })
 }
