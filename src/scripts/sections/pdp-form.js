d38bd9ac927e8647caf4bf2a39bcf9f1510bb1dc
Max <max.rolon@barrelny.com>
2019-03-07 22:07:21 -0500

    WIP Refactor of pdp-form (no linting issues)

--- src/scripts/sections/pdp-form.js
+++ src/scripts/sections/pdp-form.js
@@ -1,7 +1,6 @@
 import select from 'dom-select'
 import on from 'dom-event'
 
-/* eslint-disable */
 class ProductForm {
   constructor (el) {
     this.$el = el
@@ -10,12 +9,14 @@ class ProductForm {
     this.$swatches = select.all('.js-pdp-swatch', el)
     this.$nonSwatchInputs = select.all('.js-pdp-non-swatch-input', el)
     this.$addToCartBtn = select('.js-add-to-cart', el)
+    this.$colorLabelContainers = select.all('.js-pdp-selected-color', el)
 
     this.json = JSON.parse(
-      select('[data-product-json]').innerHTML().trim()
+      select('[data-product-json]').innerHTML.trim()
     )
     this.siblingsJson = window.siblingsJson
-    this.siblingId = ''
+
+    this.productID = ''
 
     this.bindContexts()
     this.updateWaitlistMeta()
@@ -32,6 +33,12 @@ class ProductForm {
     this.onAddToCart = this.onAddToCart.bind(this)
   }
 
+  updateProductID (id) {
+    this.productID = id
+    this.variants = this.siblingsJson[id].variants
+    this.product = this.siblingsJson[id]
+  }
+
   attachEventListeners () {
     this.$nonSwatchInputs.forEach(el => (
       on(el, 'click', this.onNonSwatchChange)
@@ -45,111 +52,113 @@ class ProductForm {
 
   onSwatchChange (e) {
     e.preventDefault()
-    this.updatePricing(siblingId)
+
+    const $target = e.target
+    const productID = $target.getAttribute('data-sibling')
+    const label = select('img', $target).getAttribute('data-color-label')
+
+    this.updateProductID(productID)
+    this.updateVariantInput()
+    this.updatePricing(productID)
     this.updateCta()
     this.updateDataLayer()
     this.updateURL(url)
     this.updateLowStockWarning()
     this.updateFinalSaleMessage()
     this.updateWaitlistMeta()
+    this.updateActiveProduct(target)
+    this.updateLabels($target)
 
-    var siblingId = $(this).data('sibling');
-    var currentSize = $('#variant-buttons .selected').text();
+    this.$swatches.forEach(el => (
+      el.classlist.removeClass('active')
+    ))
+    $target.classlist.add('active')
+  }
 
-    // update active swatch
-    $('.pdp-swatch').removeClass('active');
-    $(this).addClass('active');
+  updateVariantInput () {
+    this.$variantInput.innerHTML = ''
+    $('.js-variant-buttons-wrap').html('')
+    this.variants.forEach(variant => {
+      $(this.$variantInput).append(
+        `<option value="${variant.id}">${variant.title}</option>`
+      )
 
-    // get the selected color
-    var selectedColor =  $(this).find('img').data('color-label');
-
-    // find out which option has been clicked (option 1 or 2)
-    var optionNumber = $('.pdp-swatches').index($(this).parents('.pdp-swatches'));
-
-    // update selected color in correpondent option
-    $('.configuration-label .pdp-selected-color').text('');
-    $('.configuration-label').eq(optionNumber).find('.pdp-selected-color').text(selectedColor);
-
-    // configuration-label
-
-
-    $('#current-option span').text( $(this).find('img').data('color-label') );
-
-    // replace options in our Size selector. hard coded for single option variants
-    var newVariants = siblingsJson[siblingId].variants;
-    $('#variant-buttons').html('');
-    $variants.html('');
-    for (var i = 0; i <= newVariants.length - 1; i++) {
-      // build our hidden select
-      $variants.append('<option value="'+newVariants[i].id+'">'+newVariants[i].title+'</option>');
-
-      // build our buttons, hiding some based on their out-of-stock-policy metafield
-      if( hiddenVariants.indexOf(newVariants[i].id) == -1 ){
-        $('#variant-buttons').append('<a href="#" data-id="'+newVariants[i].id+'" class="btn btn-secondary variant-option">'+newVariants[i].title+'</a>');
+      // hidden variants??
+      if (!~hiddenVariants.indexOf(variant.id)) {
+        $('.js-variant-buttons-wrap').append(
+          `<a
+            href="#"
+            data-id="${variant.id}"
+            class="btn btn-secondary variant-option">
+            ${variant.title}
+          </a>`
+        )
       }
-    };
+    })
+  }
 
-    // reselect the size, or use the first option
-    var sizeFound = false;
-    $('#variant-buttons .btn').each(function(){
-      if( $(this).text() == currentSize ){
-        sizeFound = true;
-        $(this).click();
+  updateLabels ($swatch) {
+    this.colorLabelContainers.forEach(el = (el.innerHTML = ''))
+    const $container = slate.utils.getClosest($swatch, '.js-pdp-swatches')
+    select('.js-pdp-selected-color', $container).innerHTML = label
+  }
+
+  /**
+   * Replace options in our Size selector. hard coded for single option variants
+   * Reselect the size, or use the first option
+   */
+  updateSizeSelection ($target) {
+    const currentSize = $('.js-variant-buttons-wrap .selected').text()
+    let sizeFound = false
+    $('.js-variant-buttons-wrap .btn').each(() => {
+      if ($(this).text() !== currentSize) {
+        return
       }
-    });
-    if( !sizeFound ){
-      $('#variant-buttons .btn').first().click();
+      sizeFound = true
+      $target.click()
+    })
+    if (!sizeFound) {
+      $('.js-variant-buttons-wrap .btn').first().click()
     }
-
-    // update waitlist
-    $('#wl-image').attr('src', siblingsJson[siblingId].featured_image);
-    $('#wl-product').val(siblingId);
-
-    // replace content
-    $('.product-detail-panel').each(function(){
-      var $overrideContent = $(this).find('[data-sibling="'+siblingId+'"]');
-      if( $overrideContent.length ){
-        $(this).find('.default-content, .sibling-content').addClass('hide');
-        $overrideContent.removeClass('hide');
-      }else{
-        $(this).find('.sibling-content').addClass('hide');
-        $(this).find('.default-content').removeClass('hide');
-      }
-    });
   }
 
   onNonSwatchChange (e) {
-    e.preventDefault();
-    var $btn = $(this);
-    $('.variant-buttons .validation-error').remove();
-    $btn.closest('.variant-buttons').find('.variant-option').removeClass('selected');
-    $btn.addClass('selected');
-    if( !$(this).hasClass('gc-option') ){ // size attributes
-      $variants.find('option').each(function(){
-        if( $(this).text().trim() == $btn.text() ){
-          $variants.val($(this).attr('value'));
-          $variants.trigger('change');
+    e.preventDefault()
+    const $target = e.target
+
+    $('.variant-buttons .validation-error').remove()
+
+    $btn.closest('.variant-buttons').find('.variant-option').removeClass('selected')
+    $btn.addClass('selected')
+
+    if (!$(this).hasClass('gc-option')) { // size attributes
+      $variants.find('option').each(() => {
+        if ($(this).text().trim() === $btn.text()) {
+          $variants.val($(this).attr('value'))
+          $variants.trigger('change')
         }
-      });
-      $('#wl-variant').val($variants.val());
-      updateCta();
-      updateDataLayer();
-      updateWaitlistMeta();
-      updateLowStockWarning();
-      updateFinalSaleMessage();
-    }else{ // gift card options
-        var selectedString = getSelectedString();
-      $variants.find('option').each(function(){
-        if( $(this).text().trim() == selectedString ){
-          $variants.val($(this).attr('value'));
+      })
+
+      $('#wl-variant').val($variants.val())
+
+      this.updateCta()
+      this.updateDataLayer()
+      this.updateWaitlistMeta()
+      this.updateLowStockWarning()
+      this.updateFinalSaleMessage()
+    } else { // gift card options
+      const selectedString = getSelectedString()
+      $variants.find('option').each(() => {
+        if ($(this).text().trim() === selectedString) {
+          $variants.val($(this).attr('value'))
         }
-      });
+      })
       // update the featured image and hidden image field
-      for (var i = 0; i < productJson.variants.length; i++) {
-        if( productJson.variants[i].id == $variants.val() ){
-          var variantImage = productJson.variants[i].featured_image.src;
-          $('[data-product-featured-image]').attr('src', variantImage);
-          $('[data-variant-image]').val(variantImage);
+      for (let i = 0; i < productJson.variants.length; i++) {
+        if (productJson.variants[i].id === this.$variantInput.value) {
+          const variantImage = productJson.variants[i].featured_image.src
+          $('[data-product-featured-image]').attr('src', variantImage)
+          $('[data-variant-image]').val(variantImage)
         }
       };
     }
@@ -185,13 +194,13 @@ class ProductForm {
   }
 
   updateURL (url) {
-    if ( history.replaceState ) {
+    if (history.replaceState) {
       const newurl = window.location.protocol + '//' + window.location.host + url
-      window.history.replaceState({path: newurl}, '', newurl)
+      window.history.replaceState({ path: newurl }, '', newurl)
       sessionStorage.setItem('lo-back-to', newurl) // for use in the cart
 
       dataLayer.push({
-        'event' : 'afterUrlUpdate'
+        'event': 'afterUrlUpdate'
       })
     }
   }
@@ -216,7 +225,7 @@ class ProductForm {
 
     if (Number(compare)) {
       const formated = slate.Currency.formatMoney(compare, theme.moneyFormat)
-      $discount.innerHTML = Math.round( 100 * (compare - price) / compare )
+      $discount.innerHTML = Math.round(100 * (compare - price) / compare)
       $badges.forEach(el => el.classlist.remove('is-hidden'))
       $compare.classlist.remove('is-hidden')
       $compare.innerHTML = formattedCompare
@@ -225,36 +234,36 @@ class ProductForm {
       $badges.forEach(el => el.classlist.add('is-hidden'))
     }
 
-    $price.innerHTML = formattedPrice.replace('.00','')
+    $price.innerHTML = formattedPrice.replace('.00', '')
   }
 
   updateCta () {
-    var selectedVariantID = $variants.val();
-    var $soldOutMsg = $('#sold-out-message');
-    var $addToCartRow = $('#add-to-cart');
-    var $addToCartBtn = $('[data-add-to-cart]');
-    var $addToWaitlistBtn = $('[data-add-to-waitlist]');
-    var siblingId = $('.pdp-swatch.active').data('sibling');
-    var newVariants = siblingsJson[siblingId].variants;
+    const selectedVariantID = this.$variantInput.value
+    const $soldOutMsg = $('#sold-out-message')
+    const $addToCartRow = $('#add-to-cart')
+    const $addToCartBtn = $('[data-add-to-cart]')
+    const $addToWaitlistBtn = $('[data-add-to-waitlist]')
+    const siblingId = $('.pdp-swatch.active').data('sibling')
     // reset to purchase or waitlist
-    $addToCartRow.show();
-    $soldOutMsg.hide();
-    for (var i = 0; i < newVariants.length; i++) {
-      if( newVariants[i].id == selectedVariantID ){
-        if( newVariants[i].available ){
+    $addToCartRow.show()
+    $soldOutMsg.hide()
+
+    for (var i = 0; i < this.variants.length; i++) {
+      if (this.variants[i].id === selectedVariantID) {
+        if (this.variants[i].available) {
           // can buy
-          $addToCartBtn.show();
-          $addToWaitlistBtn.hide();
-        }else{
-          if( variantStockData[selectedVariantID].oosPolicy == 'soldout'){
+          $addToCartBtn.show()
+          $addToWaitlistBtn.hide()
+        } else {
+          if (variantStockData[selectedVariantID].oosPolicy === 'soldout') {
             // can't buy
-            $addToCartRow.hide();
-            $soldOutMsg.show();
-          }else{
+            $addToCartRow.hide()
+            $soldOutMsg.show()
+          } else {
             // can buy later
-            $addToWaitlistBtn.data('variant-id', selectedVariantID).show();
-            $addToCartBtn.hide();
-            $('#wl-variant').val(selectedVariantID);
+            $addToWaitlistBtn.data('variant-id', selectedVariantID).show()
+            $addToCartBtn.hide()
+            $('#wl-variant').val(selectedVariantID)
           }
         }
       }
@@ -262,39 +271,43 @@ class ProductForm {
   }
 
   updateWaitlistMeta () {
-    var wlMeta = [$('#current-option span').text()];
-    if( $('#variant-buttons ').length ){
-      wlMeta.push($('#variant-buttons .selected').text());
+    $('#wl-image').attr('src', this.product.featured_image)
+    $('#wl-product').val(this.productID)
+    const wlMeta = [$('#current-option span').text()]
+    if ($('#variant-buttons ').length) {
+      wlMeta.push(
+        $('#variant-buttons .selected').text()
+      )
     }
-    $('[data-wl-meta]').text(wlMeta.join(' - '));
+    $('[data-wl-meta]').text(wlMeta.join(' - '))
 
-    var variant = $variants.val();
-    var wlExpected = variantStockData[variant].restockMessage;
-    if( wlExpected != '' ){
-      $('[data-wl-expected]').text('Expected in stock: ' + wlExpected);
-    }else{
-      $('[data-wl-expected]').text('');
+    const variant = this.$variantInput.value
+    const wlExpected = variantStockData[variant].restockMessage
+    if (wlExpected) {
+      $('[data-wl-expected]').text('Expected in stock: ' + wlExpected)
+    } else {
+      $('[data-wl-expected]').text('')
     }
   }
 
   updateDataLayer () {
-    var dataLayer = window.dataLayer || []
-    var selectedSibling = $('.pdp-swatch.active').data('sibling')
-    var selectedVariantID = this.$variantInput.value
+    const dataLayer = window.dataLayer || []
+    const selectedSibling = $('.pdp-swatch.active').data('sibling')
+    const selectedVariantID = this.$variantInput.value
     for (var i = 0; i < siblingsJson[selectedSibling].variants.length; i++) {
-      if( siblingsJson[selectedSibling].variants[i].id == selectedVariantID ) {
+      if (siblingsJson[selectedSibling].variants[i].id === selectedVariantID) {
         dataLayer.push({
-          'event' : 'swatchClick',
-          'productData.price' : siblingsJson[selectedSibling].variants[i].price * 0.01,
-          'productData.comparePrice' : siblingsJson[selectedSibling].variants[i].compare_at_price * 0.01,
-          'productData.sku' : siblingsJson[selectedSibling].variants[i].sku,
-          'productData.name' : siblingsJson[selectedSibling].title,
-          'productData.variant' : siblingsJson[selectedSibling].variants[i].title,
-          'productData.variantId' : selectedVariantID,
-          'productData.url' : siblingsSupplimentalJson[selectedSibling].url,
-          'productData.imageUrl' : siblingsSupplimentalJson[selectedSibling].featuredImage,
-          'productData.collections' : siblingsSupplimentalJson[selectedSibling].collections,
-          'pinterestPage' : siblingsJson[selectedSibling].title
+          'event': 'swatchClick',
+          'productData.price': siblingsJson[selectedSibling].variants[i].price * 0.01,
+          'productData.comparePrice': siblingsJson[selectedSibling].variants[i].compare_at_price * 0.01,
+          'productData.sku': siblingsJson[selectedSibling].variants[i].sku,
+          'productData.name': siblingsJson[selectedSibling].title,
+          'productData.variant': siblingsJson[selectedSibling].variants[i].title,
+          'productData.variantId': selectedVariantID,
+          'productData.url': siblingsSupplimentalJson[selectedSibling].url,
+          'productData.imageUrl': siblingsSupplimentalJson[selectedSibling].featuredImage,
+          'productData.collections': siblingsSupplimentalJson[selectedSibling].collections,
+          'pinterestPage': siblingsJson[selectedSibling].title
         })
       }
     }
@@ -304,9 +317,9 @@ class ProductForm {
     const variant = this.$variantInput.value
     const inventoryLevel = variantStockData[variant].stockLevel
     const $warning = $('.low-stock-warning')
-    if( inventoryLevel > 0 && inventoryLevel <= 20 ){
-      $warning.addClass('shown');
-    }else{
+    if (inventoryLevel > 0 && inventoryLevel <= 20) {
+      $warning.addClass('shown')
+    } else {
       $warning.removeClass('shown')
     }
   }
@@ -314,7 +327,7 @@ class ProductForm {
   updateFinalSaleMessage () {
     const variant = this.$variantInput.value
     const $warning = $('.final-sale-warning')
-    if ( discontinued.includes(variant) ) {
+    if (discontinued.includes(variant)) {
       $warning.addClass('shown')
     } else {
       $warning.removeClass('shown')
@@ -322,9 +335,9 @@ class ProductForm {
   }
 
   getSelectedString () {
-    var selectedText = [];
-    $('.variant-option').each(function () {
-      if ( $(this).hasClass('selected') ) { // selected status get set in swatches.js
+    var selectedText = []
+    $('.variant-option').each(() => {
+      if ($(this).hasClass('selected')) { // selected status get set in swatches.js
         selectedText.push($(this).text().trim())
       }
     })
@@ -336,9 +349,9 @@ class ProductForm {
    */
   scrollToYotpo () {
     $('.pdp-main-product-yotpo').click(e => {
-      $('html, body').animate({scrollTop: (
+      $('html, body').animate({ scrollTop: (
         $('#reviews-widget').offset().top - 200
-      )}, 1000)
+      ) }, 1000)
     })
   }
 }
@@ -346,4 +359,3 @@ class ProductForm {
 export default el => {
   return new ProductForm(el)
 }
-/* eslint-enable */
