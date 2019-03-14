687122eef4e6e16a2076eeab37ea4769eb8c65ef
Max <max.rolon@barrelny.com>
2019-03-14 10:04:06 -0400

    Removes Flickity from stories

--- src/scripts/sections/pdp-stories.js
+++ src/scripts/sections/pdp-stories.js
@@ -1,14 +1,1 @@
-import Flickity from 'flickity'
-
-export default el => {
-  if (!el) return
-  var flkty = new Flickity(el, {
-    draggable: '>1',
-    groupCells: true,
-    contain: false,
-    watchCSS: true,
-    pageDots: false,
-    prevNextButtons: false,
-    freeScroll: true
-  })
-}
+export default el => {}
