0fc836d2b01c2c074bc657928d0812c2c27174a9
Max <max.rolon@barrelny.com>
2019-03-17 14:00:16 -0400

    Updates objectFit shim and image sizes

--- src/scripts/sections/pdp-image.js
+++ src/scripts/sections/pdp-image.js
@@ -33,34 +33,14 @@ const objectFitShim = (el) => {
   }
   // We'll compare the aspect ratio of media items with the aspect ratio of the parent container to decide how
   const elAspect = el.clientHeight / el.clientWidth
-  const elMediaItems = el.querySelectorAll('img,video')
+  const elMediaItem = select('img', el)
+  const src = elMediaItem.getAttribute('data-normal') || elMediaItem.getAttribute('src')
+  const mediaClass = elMediaItem.getAttribute('data-js-class')
 
-  // We need to hide overflow for the shim to properly replicate object-fit:cover
-  el.style.overflow = 'hidden'
+  elMediaItem.classList.add('hidden')
 
-  // Grab each media-item and adjust its class-names
-  for (var i = 0; i < elMediaItems.length; i++) {
-    let mediaItem = elMediaItems[i]
-    let mediaAspect = mediaItem.clientHeight / mediaItem.clientWidth
-    let mediaClass = mediaItem.getAttribute('data-js-class')
-
-    mediaItem.className = mediaItem.className.replace(mediaClass, mediaClass + '--shim')
-
-    setTimeout(() => { // TODO: how to handle transforms
-      // add a class to some things that can make them transform
-    }, 10)
-
-    // Establish which edge of the mdia will hit container last (horizontal or vertical)
-    // This is used specifically for the object-fit:cover shim, but both max-width:100%
-    // and max-height:100% are necessary (and already established) for the
-    // object-fit:contain shim, so why build the logic to scope it?
-    if (mediaAspect <= elAspect) {
-      mediaItem.style.maxWidth = '100%'
-    }
-    if (mediaAspect > elAspect) {
-      mediaItem.style.maxHeight = '100%'
-    }
-  }
+  el.style.backgroundImage = `url(${src})`
+  el.classList.add('lazy-img--shim')
 }
 export default () => {
   instance
