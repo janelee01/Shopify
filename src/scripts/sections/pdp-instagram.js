ac631fa8ee6fb3eac5cd61dda8e0167de43cfdf5
Max <max.rolon@barrelny.com>
2019-03-17 13:21:01 -0400

    Fades in instagram gallery when ready

--- src/scripts/sections/pdp-instagram.js
+++ src/scripts/sections/pdp-instagram.js
@@ -1,1 +1,28 @@
-export default el => {}
+import select from 'dom-select'
+
+export default el => {
+  const $widget = select('.js-instagram-widget', el)
+
+  let interval = false
+
+  const yotpoActive = () => {
+    return !!select('.yotpo-single-image-container')
+  }
+
+  const showYotpo = () => {
+    clearTimeout(interval)
+    setTimeout(() => {
+      $widget.classList.remove('is-hidden')
+    }, 500)
+  }
+
+  const conditionallyShow = () => {
+    if (yotpoActive()) {
+      showYotpo()
+    } else {
+      interval = setTimeout(conditionallyShow, 500)
+    }
+  }
+
+  conditionallyShow()
+}
